e828939ac0cfb3dd2498d9201937c3213f590f68
ryannamgung <ryannamgung@gmail.com>
2018-08-30 14:22:41 -0400

    w

--- src/components/MyHog.js
+++ src/components/MyHog.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import { Card } from 'semantic-ui-react';
 
-function convertToJPG(hogName){
-  let nameBroken = hogName.split(" ")
+const MyHog = props => {
+
+  let nameBroken = props.hog.name.split(" ")
   nameBroken = nameBroken.map((namePart) => namePart.toLowerCase())
   let imageSrc = nameBroken.join("_")
   imageSrc += ".jpg"
-  console.log(imageSrc)
-
-  return imageSrc
-}
-
-const MyHog = props => {
-  // debugger
+  //note that we use require when we are accessing local image files
   return (
     <div>
-    <Card
-       image= "../hog-imgs/{convertToJPG(props.hog.name)}"
-       header={props.hog.name}
-       meta="Kill me now and get free bacon?"
-       description={props.hog.specialty}
-       />
+      <Card
+         image={require(`../hog-imgs/${imageSrc}`)}
+         header={props.hog.name}
+         meta="Kill me now and get free bacon!!!!"
+         description={props.hog.specialty}
+         />
     </div>
   )
 }
